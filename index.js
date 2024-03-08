d8137b932f404ae8de9cef35c049b3846c22b1be
unknown <phuongdh.tha@vnpt.vn>
2024-03-08 09:30:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import fileOperation from 'file-operations-dhp'
+
+
+fileOperation.readFile('example.txt');
+
+fileOperation.writeFile('example.txt', 'Hello, this is content to be written into the file.');
